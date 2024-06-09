2c7ba678154bf066d03cceaf8a33f788e2508b61
octav1an <octav1an@hotmail.com>
2024-06-09 21:57:07 +0200

    add views

--- src/recipe/views.py
+++ src/recipe/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient
 )
 from recipe import serializers
 
@@ -52,4 +53,18 @@ class TagViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """Filter queryset to authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin,
+                        viewsets.GenericViewSet):
+    """View for managing ingredients APIs"""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
