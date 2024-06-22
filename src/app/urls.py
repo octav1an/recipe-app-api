f222c1d0eca99aeb305d8af95221f9ea02994a1f
octav1an <octav1an@hotmail.com>
2024-06-22 17:00:56 +0200

    fix formatting

--- src/app/urls.py
+++ src/app/urls.py
@@ -13,20 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
-from django.contrib import admin
-from django.urls import include, path
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api_schema'),
-    path(
-        'api/docs/',
-        SpectacularSwaggerView.as_view(url_name='api_schema')),
-    path('api/user/', include('user.urls')),
-    path('api/recipe/', include('recipe.urls'))
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api_schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="api_schema")),
+    path("api/user/", include("user.urls")),
+    path("api/recipe/", include("recipe.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
